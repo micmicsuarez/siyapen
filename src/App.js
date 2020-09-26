779dfabb552b1656c3b68617d9ca7f310bd7015a
Michael Nico Suarez <michaelnicosuarez@gmail.com>
2020-09-26 17:48:30 +0800

    Change the title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Siyapen Tech Inc.!
+          Welcome to Siyapen!
         </p>
       </header>
     </div>
